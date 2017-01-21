788dd116fdac4bc7eda885e1ff9b76b7b640730d
HyeRyeongSong <loverhdiddl@gmail.com>
2017-01-21 16:22:52 +0900

    Thread 개념 실습

--- Practice_Code/src/Main.java
+++ Practice_Code/src/Main.java
@@ -1,44 +1,26 @@
 /**
  * Created by HyeRyeongSong on 2017. 1. 15..
  */
-interface Parent1
-{
-    public abstract void On();
-
-    public abstract void Off();
-}
-
-interface Parent2
-{
-    public abstract void Up();
-
-    public abstract void Down();
-}
-
-class TV implements Parent1, Parent2{
-    public void Up(){
-        System.out.println("Channel Up");
-    }
-    public void Down(){
-        System.out.println("Channel Down");
-    }
-    public void On(){
-        System.out.println("Channel On");
-    }
-    public void Off(){
-        System.out.println("Channel Off");
-    }
-
-}
-
 public class Main
 {
     public static void main(String[] args)
     {
-        TV tv = new TV();
-        tv.On();
-        tv.Up();
-        tv.Down();
-        tv.Off();
+        Thread t = new Thread(){
+            public void run(){
+                for(int i=1; i<10; ++i){
+                    System.out.println(i);
+                }
+            }
+        };
+        Thread t2 = new Thread(){
+            public void run(){
+                while(true){
+                    System.out.println("A");
+                }
+            }
+        };
+        t2.start();
+        t.start();
+        System.out.println("JUST");
     }
 }
